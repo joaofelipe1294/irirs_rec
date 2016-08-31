68dd990a0ec4f384cb8168b27aeadc691168ae85
Joao Felipe Lopes <joaofelipe1294@gmail.com>
2016-08-31 14:56:41 -0300

    Concluido calculo da transformada de Hough

--- hough.py
+++ hough.py
@@ -1,10 +1,10 @@
 from base_loader import BaseLoader
-from utils.pre_processor import PreProcessor
+from utils.segmentation import Segmentation
 import cv2
 import numpy as np
-import math
+import math	
 
-base = BaseLoader('CASIA-Iris-Lamp-100')
+"""base = BaseLoader('CASIA-Iris-Lamp-100')
 subject = base.subjects[0]
 image_path = subject.left_image_paths[0]
 
@@ -15,23 +15,30 @@ edges = cv2.Canny(threshold_image , 100 , 200)
 rows , cols = edges.shape[:2]
 ray = 47
 hough_image = np.zeros((rows , cols) , np.uint8)
-print('linhas : ' + str(cols))
-for i in range(0 , rows):
-	for j in range(0 , cols):
-		if edges[i][j] == 255:
+for x in range(0 , rows):
+	for y in range(0 , cols):
+		if edges[x][y] == 255:
+			pass
+			#print('ponto : ' + str(x) + ',' + str(y))
 			for teta in range(0,359):
 
 				xt = j - (ray - math.cos(teta))
 				yt = i - (ray - math.sin(teta))
-				print('xt : ' + str(int(xt)) + ' | yt : ' + str(int(yt)) + ' | i:' + str(i) + ' j:' + str(j))
-				hough_image[int(yt)][int(xt)] += 1
+				#print('xt : ' + str(int(xt)) + ' | yt : ' + str(int(yt)) + ' | i:' + str(i) + ' j:' + str(j))
+				hough_image[int(yt)][int(xt)] += 1"""
 				#print('XT : ' + str(xt) + ' | YT : ' + str(yt))
 #while i < rows - 1:
 #	print(image[i][0])
 #	i += 1
 
-cv2.imshow('hough_image' , hough_image)
-cv2.waitKey(0)
+#print(edges[0][292])
+#for teta in range(0,359):
+	#xt = 0 - ray * math.cos(teta)
+
+
+#cv2.imshow('original_image' , edges)
+#cv2.imshow('hough_image' , hough_image)
+#cv2.waitKey(0)
 
 
 #cv2.imshow('image' , image)
@@ -39,3 +46,64 @@ cv2.waitKey(0)
 #cv2.imshow('pre_processed image' , pre_processed_image)
 #cv2.imshow('edges' , edges)
 #cv2.waitKey(0)
+
+
+base = BaseLoader('CASIA-Iris-Lamp-100')
+
+subject = base.subjects[0]
+image_path = subject.left_image_paths[0]
+rgb_image = cv2.imread(image_path)
+original_image = cv2.imread(image_path , 0)
+height , width = original_image.shape[:2]
+
+
+segmentation = Segmentation(image_path)
+segmentation.pre_process()
+segmentation.houghs_transform()
+"""pre_processed_image = segmentation.pre_process()
+edges = cv2.Canny(pre_processed_image , 100 , 200)
+#cv2.imshow('rgb_image' , rgb_image)
+#cv2.imshow('edges' , edges)
+#cv2.waitKey(0)
+
+
+
+#image = np.zeros((150,150) , np.uint8)
+#image[10][74] = 255
+#image[74][10] = 255
+#image[74][139] = 255
+#image[139][74] = 255
+#print(image)
+
+hough_plan = np.zeros((height , width) , np.uint8)
+ray = 47
+for line in range(0,height):
+	for column in range(0,width):
+		if edges[line][column] == 255:
+			for teta in range(0 , 360):
+				xt = line - ray * math.cos(teta)
+				yt = column - ray * math.sin(teta)
+				#print(str(line) + '|' + str(column) + ' | teta : ' + str(teta) + ' | xt : ' + str(int(xt)) + ' | ' + str(int(yt)))
+				if int(xt) < height and int(yt) < width and int(xt) > 0 and int(yt) > 0:
+					hough_plan[int(xt)][int(yt)] += 1
+
+bigger = 0
+bigger_line = 0
+bigger_collumn = 0
+for line in range(0,height):
+	#print(line)
+	for column in range(0,width):
+		#print(column)
+		if hough_plan[line][column] >= bigger:
+			bigger = hough_plan[line][column]
+			bigger_line = line
+			bigger_collumn = column
+			#print(str(line) + ' | ' + str(column) + ' | value : ' + str(bigger))
+			#cv2.circle(rgb_image , tuple([bigger_line , bigger_collumn]) , ray , (0,0,255))
+
+
+cv2.circle(rgb_image , tuple([bigger_collumn , bigger_line]) , ray , (0,255,0))
+cv2.circle(rgb_image , tuple([bigger_collumn , bigger_line]) , 5 , (0,0,255) , -1)
+#cv2.imshow('hough_plan' , hough_plan)
+cv2.imshow('image' , rgb_image)
+cv2.waitKey(0)"""
---
+++ utils/segmentation.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+import math
+
+
+class Segmentation:
+		
+	def __init__(self , image_path):
+		self.image_path = image_path
+		self.original_image = cv2.imread(self.image_path , 0)
+		self.pre_processed_image = None
+		self.pupil_centroid = None
+		self.space_off = 250
+
+	#@classmethod
+	#def process(PreProcessor , image_path):
+	def pre_process(self):
+		threshold_image = cv2.threshold(self.original_image , 28 , 255 , cv2.THRESH_BINARY)[1] #making threshold
+		invert_threshold = cv2.bitwise_not(threshold_image)                      #inverting threshold result
+		floodfill_image = threshold_image.copy()                                 #creating the image that will be flooded
+		h, w = threshold_image.shape[:2]                                         #getting dimension values to create the mask
+		mask = np.zeros((h + 2 , w + 2) , np.uint8)                              #creating the mask that will be used on cv2.floodFill()
+		seed = (0 , 0)
+		seeds = [(0 , 0) , (319 , 0) , (639 , 0) , (0 , 139) , (639 , 139) , (0 , 479) , (319 , 479) , (639 , 479)]
+		for index in range(1 ,8):
+			cv2.floodFill(floodfill_image , mask , seeds[index] , 0)                      #appling floodFill algorithm	
+		white_pupil = invert_threshold + floodfill_image                         #filling pupil 
+		black_pupil = cv2.bitwise_not(white_pupil)                               #invert white_pupil image
+		#and_processed_image = image & black_pupil                                #using logig operation AND between the initial image and the black_pupil to complete the pre processing	
+		kernel = np.ones((9 , 9) , np.uint8)
+		open_image = cv2.morphologyEx(black_pupil , cv2.MORPH_OPEN , kernel)
+		out = open_image & black_pupil
+		self.pre_processed_image = self.original_image & out
+		#cv2.imshow('opening' , open_image)
+		#cv2.imshow('out' ,image & out)
+		#cv2.imshow('original' , image)
+		#cv2.imshow('threshold' , threshold_image)
+		#cv2.imshow('invert threshold' , invert_threshold)
+		#cv2.imshow('floodfill' , floodfill_image)
+		#cv2.imshow('white pupil' , white_pupil)
+		#cv2.imshow('black pupil' , black_pupil)
+		#cv2.imshow('pre_processed' , pre_processed_image)
+		#cv2.waitKey(0)
+		
+
+
+
+	def houghs_transform(self):
+		rgb_image = cv2.imread(self.image_path)                      #reading image that can show the circle that identify the pupil
+		height , width = self.original_image.shape[:2]          #getting number of lines and collumns ,will be used to build houghs_plan
+		edges = cv2.Canny(self.pre_processed_image , 100 , 200) #using Canny algorithm to generate the edges
+		hough_plan = np.zeros((height , width) , np.uint8)      #creating houghs_plan filled with 0
+		ray = 47                                                #defining a value for the circunference ray
+		for line in range(0,height):                            #loop that compute houghs_plan
+			for column in range(0,width):
+				if edges[line][column] == 255:
+					for teta in range(0 , 360):
+						xt = line - ray * math.cos(teta)
+						yt = column - ray * math.sin(teta)
+						if int(xt) < height and int(yt) < width and int(xt) > 0 and int(yt) > 0:
+							hough_plan[int(xt)][int(yt)] += 1
+					
+		bigger = 0             
+		bigger_line = 0
+		bigger_collumn = 0
+		for line in range(0,height):                            #loop that sweeps intire houghs_plan to find the pupil_centroid
+			for column in range(0,width):
+				if hough_plan[line][column] >= bigger:
+					bigger = hough_plan[line][column]
+					bigger_line = line
+					bigger_collumn = column
+		self.pupil_centroid = tuple([bigger_collumn , bigger_line])
+		#cv2.imshow('rgb_image' , rgb_image)
+		#cv2.imshow('edges' , edges)
+		#cv2.circle(rgb_image , tuple([bigger_collumn , bigger_line]) , ray , (0,255,0))
+		#cv2.circle(rgb_image , tuple([bigger_collumn , bigger_line]) , 5 , (0,0,255) , -1)
+		#cv2.imshow('hough_plan' , hough_plan)
+		#cv2.imshow('image' , rgb_image)
+		#cv2.waitKey(0)
+		
+		
+		
+
+		
